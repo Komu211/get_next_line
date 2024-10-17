d7544ad2f15ef79b57f5d3c6c393f8120cf897ba
Konrad Muhlbauer <kmuhlbau@1-E-26.42heilbronn.de>
2024-10-17 17:26:11 +0200

    Added strjoin to utils, to be able to expand/create bigger array, when no null terminator or line break can be found in the array to read more data

--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -6,7 +6,7 @@
 /*   By: kmuhlbau <kmuhlbau@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/16 12:20:38 by kmuhlbau          #+#    #+#             */
-/*   Updated: 2024/10/17 15:39:19 by kmuhlbau         ###   ########.fr       */
+/*   Updated: 2024/10/17 17:24:53 by kmuhlbau         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -44,10 +44,35 @@ void	ft_strlcpy(char *dst, const char *src, size_t size)
 
 size_t	ft_strlen(const char *s)
 {
-	int i;
+	int	i;
 
 	i = 0;
 	while (s[i] != '\0')
 		i++;
 	return (i);
+}
+
+char	*ft_strjoin(char const *s1, char const *s2)
+{
+	char	*jstr;
+	int		i;
+	int		len;
+
+	len = ft_strlen(s1) + ft_strlen(s2);
+	jstr = malloc((len + 1) * sizeof(char));
+	i = 0;
+	if (!jstr)
+		return (NULL);
+	while (i < len && s1[i] != '\0')
+	{
+		jstr[i] = s1[i];
+		i++;
+	}
+	while (i < len)
+	{
+		jstr[i] = s2[i - ft_strlen(s1)];
+		i++;
+	}
+	jstr[i] = '\0';
+	return (jstr);
 }

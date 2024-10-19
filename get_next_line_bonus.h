f810f41f34af24f3f8939959bc1d2d1e718cfaf0
Konrad Muhlbauer <kmuhlbau@1-E-22.42heilbronn.de>
2024-10-19 17:07:18 +0200

    Start working on Bonus

---
+++ get_next_line_bonus.h
@@ -0,0 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_bonus.h                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kmuhlbau <kmuhlbau@student.42heilbronn.    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/19 17:04:57 by kmuhlbau          #+#    #+#             */
+/*   Updated: 2024/10/19 17:06:27 by kmuhlbau         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_BONUS_H
+# define GET_NEXT_LINE_H
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE = 1024
+# endif
+
+#endif
